4507e217504a3d78a22492bf80480afa8dbed48c
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2023-08-15 14:30:53 +0300

    feat: GET single thread, update onboarding

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -34,3 +34,65 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+  const skipAmount = (pageNumber - 1) * pageSize;
+  const postQuery = Thread.find({
+    parentId: { $in: [null, undefined] },
+  })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+  const posts = await postQuery.exec();
+  const isNext = totalPostsCount > skipAmount + posts.length;
+  return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  connectToDB();
+  try {
+    // TODO: Populate Community
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`);
+  }
+}
