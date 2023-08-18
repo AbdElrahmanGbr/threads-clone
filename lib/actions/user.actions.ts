4a6d5193fbe9c752c2b285395aa251ba02fabd25
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2023-08-19 00:30:37 +0300

    feat: user profile, ThreadsTab

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { connectToDB } from "@/lib/mongoose";
 import User from "@/lib/models/user.model";
 import { revalidatePath } from "next/cache";
+import Thread from "@/lib/models/thread.model";
 
 interface Params {
   userId: string;
@@ -51,5 +52,27 @@ export async function fetchUser(userId: string) {
     // });
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+    const threads = await User.findOne({ id: userId }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+    return threads;
+  } catch (error: any) {
+    throw new Error(`Error fetching all posts: ${error.message}`);
   }
 }
